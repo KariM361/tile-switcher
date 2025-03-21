01df61b86366ae16866ddf0f17a168d8c65ddb2d
Kari idskov holm Madsen <kama7@elev.techcollege.dk>
2025-03-21 09:18:54 +0100

    Tile switch opgave

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,56 +1,43 @@
 // write cool JS hwere!!
-let mainImage = document.getElementById('mainImage')
+let mainImage = document.getElementById('mainImage'); //kalder på mainImage id'et i html
 
-let smallImageOne = document.getElementById('imgOne')
-let smallImageTwo = document.getElementById('imgTwo')
-let smallImageThree = document.getElementById('imgThree')
-let smallImageFour = document.getElementById('imgFour')
-
-
-
-
+let smallImageOne = document.getElementById('imgOne'); //kalder på imgOne id'et i html
+let smallImageTwo = document.getElementById('imgTwo'); //kalder på imgTwo id'et i html
+let smallImageThree = document.getElementById('imgThree'); //kalder på imgThree id'et i html
+let smallImageFour = document.getElementById('imgFour'); //kalder på imgFour id'et i html
 
 smallImageOne.addEventListener('click', function (event) {
+  //eventlistener der lytter efter klik på smallImageOne
+  let bigImageUrl = mainImage.src; // finder url'en på mainImage(stort billede)
+  let smallImageUrl = smallImageOne.src; //finder url'en på smallImageOne(lille billede)
 
-    let bigImageUrl = mainImage.src
-    let smallImageUrl = smallImageOne.src
-
-
-    mainImage.src = smallImageUrl
-    smallImageOne.src = bigImageUrl
-
-})
-
+  mainImage.src = smallImageUrl; //skifter stort billede med lille billede
+  smallImageOne.src = bigImageUrl; //skifter lille billed med stort billede
+});
 
 smallImageTwo.addEventListener('click', function (event) {
+  //eventlistener der lytter efter klik på smallImageTwo
+  let bigImageUrl = mainImage.src; // finder url'en på mainImage(stort billede)
+  let smallImageUrl = smallImageTwo.src; //finder url'en på smallImageTwo(lille billede)
 
-    let bigImageUrl = mainImage.src
-    let smallImageUrl = smallImageTwo.src
-
-
-    mainImage.src = smallImageUrl
-    smallImageTwo.src = bigImageUrl
-
-})
+  mainImage.src = smallImageUrl; //skifter stort billede(mainImage) med lille billede
+  smallImageTwo.src = bigImageUrl; //skifter lille billed(smallImage) med stort billede
+});
 
 smallImageThree.addEventListener('click', function (event) {
+  //eventlistener der lytter efter klik på smallImageThree
+  let bigImageUrl = mainImage.src; // finder url'en på mainImage(stort billede)
+  let smallImageUrl = smallImageThree.src; //finder url'en på smallImageThree(lille billede)
 
-    let bigImageUrl = mainImage.src
-    let smallImageUrl = smallImageThree.src
-
-
-    mainImage.src = smallImageUrl
-    smallImageThree.src = bigImageUrl
-
-})
+  mainImage.src = smallImageUrl; //skifter stort billede med lille(mainImage) billede
+  smallImageThree.src = bigImageUrl; //skifter lille billed(smallImage) med stort billede
+});
 
 smallImageFour.addEventListener('click', function (event) {
+  //eventlistener der lytter efter klik på smallImageFour
+  let bigImageUrl = mainImage.src; // finder url'en på mainImage(stort billede)
+  let smallImageUrl = smallImageFour.src; //finder url'en på smallImageFour(lille billede)
 
-    let bigImageUrl = mainImage.src
-    let smallImageUrl = smallImageFour.src
-
-
-    mainImage.src = smallImageUrl
-    smallImageFour.src = bigImageUrl
-
-})
+  mainImage.src = smallImageUrl; //skifter stort billede(mainImage)  med lille billede
+  smallImageFour.src = bigImageUrl; //skifter lille billed(smallImage) med stort billede
+});
